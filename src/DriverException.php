5558abd568ce4e19591ac0bdb0dc469455e9aa4c
Zcasper <testuser@mail.linpax.org>
2017-06-08 06:44:14 +0700

    initial

---
+++ src/DriverException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link https://github.com/linpax/microphp-driver
+ * @copyright Copyright &copy; 2017 Oleg Lunegov
+ * @license https://github.com/linpax/microphp-driver/blob/master/LICENSE
+ */
+
+namespace Micro\Driver;
+
+
+interface DriverException
+{
+
+}
